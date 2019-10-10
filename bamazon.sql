10db807fa53cd0039997ea65a47beb74c90c85c1
Tom Cox <thomasxcox731@gmail.com>
2019-10-10 14:09:25 -0500

    Roots of the js file have been written. Table rendered in mysql

--- bamazon.sql
+++ bamazon.sql
@@ -6,10 +6,22 @@ USE bamazon;
 
 CREATE TABLE products (
 id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) NULL,
-  department VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
+  name VARCHAR(45) NOT NULL,
+  department VARCHAR(45) NOT NULL,
+  price INT NOT NULL,
   quantity INT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (name, department, price, quantity)
+VALUES ("Prophet 08", "Synthesizers", 50, 5), 
+("Omnichord OM-300", "Synthesizers", 23, 3), 
+("AKAI MPC-2000XL", "Samplers", 44, 6), 
+("SP-505", "Samplers", 31, 9),
+("Fostex E-16", "Tape Machines", 53, 2), 
+("Tascam 38", "Tape Machines", 45, 8), 
+("Moog Drive", "Pedals", 12, 12), 
+("Ghost Echo", "Pedals", 11, 2), 
+("Univox SR-55", "Drum Machines", 32, 6), 
+("Tascam 32", "Tape Machines", 22, 4);
 
