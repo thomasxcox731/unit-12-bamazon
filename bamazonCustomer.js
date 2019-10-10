93e72f9e527447436f67b34f57220930a5f11fa3
Tom Cox <thomasxcox731@gmail.com>
2019-10-10 14:12:18 -0500

    Node modules installed, including inquirer and mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,27 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  // Your port; if not 3306
-  port: 3306,
-  // Your username
-  user: "root",
-  // Your password
-  password: "",
-  database: "bamazon"
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     afterConnection();
-  });
+});
 
 function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    }); 
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
