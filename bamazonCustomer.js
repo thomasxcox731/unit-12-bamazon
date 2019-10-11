402cc2ff3e577f5e0f52fc1e58ff7ce541ce6cbe
Tom Cox <thomasxcox731@gmail.com>
2019-10-11 00:09:11 -0500

    As far as I can take it for now.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,42 @@ connection.connect(function (err) {
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].name + " | " + res[i].department + " | " + res[i].price + " | " + res[i].quantity);
+
+        };
+        inquirer.prompt([
+
+            {
+                type: "list",
+                name: "products",
+                message: "What would you like to buy?",
+                choices: [res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[9]]
+            },
+        
+            {
+                type: "input",
+                name: "units",
+                message: "How many would you like?"
+            }
+        
+            ]).then(function (user) {
+                //I haven't found the right syntax to reference the items from the SQL list back here in the js. I've pseudocoded it to the best of my ability, but without that part of the syntax, I won't be able to get my app functional.
+                if(user.products.quantity < user.units){
+                    console.log("Insufficient Quantity!");
+                };
+                if(user.products.quantity >= user.units){
+                    function Update(){
+                        connection.query("UPDATE products SET quantity = user.products.quantity-1 WHERE id=user.products"), function (err, res){
+                            if (err) throw err;
+                        }
+                    };
+                    Update();
+                    console.log("Your total will be: " + user.products.price);
+                }
+            });
         connection.end();
     });
-}
+};
+
+
